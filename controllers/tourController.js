335210c68e2e0c74887c596be7a36be9adb39456
hryntar <hrynchuktt@gmail.com>
2024-10-21 18:46:10 +0300

    prettier & eslint

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -8,11 +8,11 @@ exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
       status: 'fail',
-      message: 'The “name” and “price” fields are required.'
-    })
+      message: 'The “name” and “price” fields are required.',
+    });
   }
   next();
-}
+};
 
 exports.checkID = (req, res, next, val) => {
   if (val * 1 > tours.length) {
@@ -36,7 +36,7 @@ exports.getAllTours = (req, res) => {
 
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
-  const tour = tours.find((tour) => tour.id === id);
+  const tour = tours.find((el) => el.id === id);
 
   res.status(200).json({
     status: 'success',
@@ -48,12 +48,12 @@ exports.getTour = (req, res) => {
 
 exports.createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+  const newTour = { id: newId, ...req.body };
   tours.push(newTour);
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
-    (err) => {
+    () => {
       res.status(201).json({
         status: 'success',
         data: {
