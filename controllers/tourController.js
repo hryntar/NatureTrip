59cd5d94bc03fb5001a14d04106b6c101070aedd
hryntar <hrynchuktt@gmail.com>
2024-10-25 22:01:28 +0300

    mongoose, connect to mongo DB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
 );
 
 exports.checkBody = (req, res, next) => {
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
+
+dotenv.config({ path: './.env' });
+
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PWD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => console.log('Connected to DB'));
 
 const port = process.env.PORT || 3000;
 app.listen(3000, () => {
