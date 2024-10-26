20e076a8eab5f0f3a085d3103ae9c4229357862f
hryntar <hrynchuktt@gmail.com>
2024-10-26 23:19:29 +0300

    sort implemenation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -10,8 +10,15 @@ exports.getAllTours = async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
     // const query = await Tour.find().where('duration').equals(5);
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     const tours = await query;
 
