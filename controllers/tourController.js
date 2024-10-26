3195647d2fc2ca4535f2c63af4837a0cd94e88cb
hryntar <hrynchuktt@gmail.com>
2024-10-26 23:05:19 +0300

    filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,17 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    const query = Tour.find(JSON.parse(queryStr));
+    // const query = await Tour.find().where('duration').equals(5);
+
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
