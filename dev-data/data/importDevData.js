2d77d46f6c6a42ad3e899c9921cb5cb91adfc293
hryntar <hrynchuktt@gmail.com>
2024-11-12 19:25:48 +0200

    modelling locations

--- dev-data/data/importDevData.js
+++ dev-data/data/importDevData.js
@@ -16,7 +16,7 @@ mongoose
   .then(() => console.log('Connected to DB'));
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+  fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'),
 );
 
 const importData = async () => {
