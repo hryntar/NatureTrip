a8316c2472dbb2681edfdff40ef66236abc75acf
hryntar <hrynchuktt@gmail.com>
2024-11-13 17:30:01 +0200

    tours and reviews virtual populating

--- models/tourModel.js
+++ models/tourModel.js
@@ -115,6 +115,14 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+// virtual populate
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id',
+})
+
 // document middleware
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, {
