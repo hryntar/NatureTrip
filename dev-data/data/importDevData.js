01de55d082e8f1ba24e8ddd385b90db4f1037e3b
hryntar <hrynchuktt@gmail.com>
2024-11-24 15:17:46 +0200

    booking model & controller

--- dev-data/data/importDevData.js
+++ dev-data/data/importDevData.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const Tour = require('./../../models/tourModel');
 const User = require('./../../models/userModel');
 const Review = require('./../../models/reviewModel');
+const Booking = require('./../../models/bookingModel');
 
 dotenv.config({ path: './.env' });
 
@@ -22,12 +23,16 @@ const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
 const reviews = JSON.parse(
   fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'),
 );
+const bookings = JSON.parse(
+  fs.readFileSync(`${__dirname}/bookings.json`, 'utf-8')
+);
 
 const importData = async () => {
   try {
     await Tour.create(tours);
     await User.create(users, { validateBeforeSave: false });
     await Review.create(reviews);
+    await Booking.create(bookings);
     console.log('Data successfully loaded!');
   } catch (error) {
     console.log(error);
@@ -40,6 +45,7 @@ const deleteData = async () => {
     await Tour.deleteMany();
     await User.deleteMany();
     await Review.deleteMany();
+    await Booking.deleteMany();
     console.log('Data successfully deleted!');
   } catch (error) {
     console.log(error);
