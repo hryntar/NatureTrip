80511bd707b83d1e9f628eb54801c358f75be252
hryntar <hrynchuktt@gmail.com>
2024-12-15 20:54:09 +0200

    booking on checkout success

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
@@ -34,3 +35,14 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     session
   });
 });
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
+});
+
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -6,4 +6,5 @@ const router = express.Router();
 
 router.get('/checkout-session/:tourId', authController.protect, bookingController.getCheckoutSession);
 
+
 module.exports = router;
